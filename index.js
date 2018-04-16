782da2b4512885c8a79ed4c8ccbd8755245cb0a1
Sandy Edwards <peterjamesedwards@gmail.com>
2018-04-16 17:27:57 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -13,3 +13,8 @@ function logShout(string) {
 function logWhisper(string) {
   console.log(string.toLowerCase());
 }
+
+function sayHiToGrandma(string) {
+(string.toLowerCase()) === string;
+  return 
+}
